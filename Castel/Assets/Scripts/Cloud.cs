64bd1f91df9eb271e2c0144fada52aea2bb9239b
Apanovich86 <apanovich.anna.itec@gmail.com>
2022-06-05 17:26:45 +0300

    castel

---
+++ Castel/Assets/Scripts/Cloud.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    [Header("Set in Inspector")] // a
+    public GameObject cloudsphere;
+    public int numSpheresMin = 6;
+    public int numSpheresMax = 10;
+    public Vector3 sphereOffsetScale = new Vector3(5, 2, 1);
+    public Vector2 sphereScaleRangeX = new Vector2(4, 8);
+    public Vector2 sphereScaleRangeY = new Vector2(3, 4);
+    public Vector2 sphereScaleRangeZ = new Vector2(2, 4);
+    public float scaleYMin = 2f;
+    private List<GameObject> spheres; // b
+    void Start()
+    {
+        spheres = new List<GameObject>();
+        int num = Random.Range(numSpheresMin, numSpheresMax); // c
+        for (int i = 0; i < num; i++)
+        {
+            GameObject sp = Instantiate<GameObject>(cloudsphere); // d
+            spheres.Add(sp);
+            Transform spTrans = sp.transform;
+            spTrans.SetParent(this.transform);
+            // Выбрать случайное местоположение
+            Vector3 offset = Random.insideUnitSphere; // e
+            offset.x *= sphereOffsetScale.x;
+            offset.у *= sphereOffsetScale.y;
+            offset.z *= sphereOffsetScale.z;
+            spTrans.localposition = offset; // f
+                                            // Выбрать случайный масштаб
+            Vector3 scale = Vector3.one; // g
+            scale.x = Random.Range(sphereScaleRangeX.x, sphereScaleRangeX.y);
+            scale.у = Random.Range(sphereScaleRangeY.x, sphereScaleRangeY.y);
+            scale.z = Random.Range(sphereScaleRangeZ.x, sphereScaleRangeZ.y);
+            scale.у *= 1 - (Mathf.Abs(offset.х) / sphereOffsetScale.x); // h
+            scale.у = Mathf.Max(scale.у, scaleYMin);
+            spTrans.localscale = scale;
+        }  
+
+    }
+    // Update вызывается в каждом кадре
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //Restart();
+        //}
+    }
+    void Restart()
+    {
+        // Удалить старые сферы, составляющие облако
+        foreach (GameObject sp in spheres)
+        {
+        Destroy(sp);
+        }
+        Start();
+    }
+}
+
---
+++ Castel/Assets/Scripts/CloudCrafter.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCrafter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Set in Inspector”)]
+ public
+ public
+ public
+ public
+ public
+ public
+ public
+ int
+ GameObject
+Vector3
+Vector3
+float
+float
+float
+// Число облаков
+// Шаблон для облаков
+numClouds = 40;
+    cloudPrefab;
+cloudPosMin = new Vector3(-50,-5,10);
+    cloudPosMax = new Vector3(150,100,10);
+    cloudScaleMin =1; // Мин. масштаб каждого облака
+cloudScaleMax = 3; // Макс, масштаб каждого облака
+cloudSpeedMult = 0.5f; // Коэффициент скорости облаков
+private GameObject[] cloudinstances;
+    void Awake()
+    {
+        // Создать массив для хранения всех экземпляров облаков
+        cloudinstances = new GameObject[numClouds];
+        // Найти родительский игровой объект CloudAnchor
+        GameObject anchor = GameObject.Find("CloudAnchor”);
+        // Создать в цикле заданное количество облаков
+        GameObject cloud;
+        for (int i = 0; i < numClouds; i++)
+        {
+            // Создать экземпляр cloudPrefab
+            cloud = Instantiate<GameObject>(cloudPrefab);
+            // Выбрать местоположение для облака
+            Vector3 cPos = Vector3.zero;
+            cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
+            cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.у);
+            // Масштабировать облако
+            float scaleU = Random.value;
+            float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
+            // Меньшие облака (с меньшим значением scaleU) должны быть ближе
+            // к земле
+            cPos.y = Mathf.Lerp(cloudPosMin.у, cPos.y, scaleU);
+            // Меньшие облака должны быть дальше
+            cPos.z = 100 - 90 * scaleU;
+            // Применить полученные значения координат и масштаба к облаку
+            cloud.transform.position = cPos;
+            cloud.transform.localscale = Vector3.one ♦ scaleVal;
+            // Сделать облако дочерним по отношению к anchor
+            cloud.transform.SetParent(anchor.transform);
+            cloudinstances[i] = cloud;
+
+
+            // Update is called once per frame
+            void Update()
+    {
+                foreach (GameObject cloud in cloudinstances)
+                {
+                    // Получить масштаб и координаты облака
+                    float scaleVal = cloud.transform.localScale.x;
+                    Vector3 cPos = cloud.transform.position;
+                    // Увеличить скорость для ближних облаков
+                    cPos.x -= scaleVal * Time.deltaTime ♦ cloudSpeedMult;
+                    // Если облако сместилось слишком далеко влево...
+                    if (cPos.x <= cloudPosMin.x)
+                    {
+                        // Переместить его далеко вправо
+                        cPos.x = cloudPosMax.x;
+                    }
+                    // Применить новые координаты к облаку
+                    cloud.transform.position = cPos;
+                }
+            }
